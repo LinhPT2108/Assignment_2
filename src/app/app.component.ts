431462a2a083606c58ef2c5e73854c6db44097dd
LinhPT2108 <tuanlinho9o22o15@gmail.com>
2023-06-07 21:19:25 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'LinhptPC04737_Assignment_2';
+}
